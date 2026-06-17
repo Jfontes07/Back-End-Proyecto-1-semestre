900d72c1968b19b581e7c3a49f5a7ad810a993f1
Jfontes07 <jonathan.fontes@anima.edu.uy>
2026-06-16 22:02:41 -0300

    conexion back parte!

---
+++ Front End/indexcaratula.js
@@ -0,0 +1,14 @@
+// URL base del back end
+const API_URL = "http://localhost:3000";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const respuesta = await fetch(`${API_URL}/articulos`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const datos = await respuesta.json();
+    console.log(datos);
+  } catch (e) {}
+});
